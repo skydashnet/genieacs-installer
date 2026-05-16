18b64d1c41dff6bc6c88cef1e9a3921c3a02e4cf
EtherGig <ethergig07@gmail.com>
2026-05-16 22:58:16 +0700

    chore: update KnownProductClass vparam with new supported devices

--- virtual-params/KnownProductClass.js
+++ virtual-params/KnownProductClass.js
@@ -1,18 +1,35 @@
 let result = "Other"
 const knownProductClass = [
+    "F460",
+    "F609",
+    "F650",
+    "F663NV3a",
+    "F663NV3A",
+    "F663NV9",
+    "F670",
+    "F670L",
+    "G663 XPON",
     "G663-XPON",
+    "G665 XPON",
+    "G680L XPON",
+    "G99 XPON",
+    "GM219",
     "GM220-S",
+    "GM220-S XPON",
     "GM630",
     "H1s-3",
+    "HG6145D2",
+    "HG6245N",
+    "HG8145V5",
+    "HG8245A",
+    "HG8245H",
+    "HG8245H5",
+    "HG8546M",
     "HS8145C5",
-  	"HG8245A",
     "MQ220",
-    "TOTOLINK_N200RE",
-    "TOTOLINK_N300RT",
-    "TOTOLINK_N355RT",
-    "ZXHN F450(EPON ONU)",
-  	"F663NV9"
-  ];
+    "XSF609",
+    "ZXHN F450(EPON ONU)"
+];
 
 let prod = declare("DeviceID.ProductClass", {value: Date.now()});
 if(knownProductClass.includes(prod.value[0])){
