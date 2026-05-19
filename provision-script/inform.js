3596ae50761d1ead28b4a1d120404a17083f2a8b
EtherGig <ethergig07@gmail.com>
2026-05-19 10:24:04 +0700

    style: apply user spacing and indent refinements to inform.js

--- provision-script/inform.js
+++ provision-script/inform.js
@@ -35,7 +35,6 @@ declare("InternetGatewayDevice.ManagementServer.Password", { value: daily }, { v
 // ==========================================
 // Configure DHCP Lease time
 declare("InternetGatewayDevice.LANDevice.1.LANHostConfigManagement.DHCPLeaseTime", { value: daily }, { value: 3600 });
-
 // Enable Huawei LAN L3
 declare("InternetGatewayDevice.LANDevice.1.LANEthernetInterfaceConfig.*.X_HW_L3Enable", { value: daily }, { value: true });
 
@@ -53,17 +52,17 @@ declare("InternetGatewayDevice.X_HW_Security.X_HW_FirewallLevel", { value: fiveM
 let autoProvisionTag = declare("Tags.AutoPPPoE", { value: 1 });
 
 if (autoProvisionTag.size > 0) {
-  let pppConn = declare("InternetGatewayDevice.WANDevice.1.WANConnectionDevice.*.WANPPPConnection.*.Enable", { value: Date.now() });
+    let pppConn = declare("InternetGatewayDevice.WANDevice.1.WANConnectionDevice.*.WANPPPConnection.*.Enable", { value: Date.now() });
 
-  if (pppConn.size === 0) {
-    log("GenieACS Auto-Provisioning: Tagged device missing PPPoE WAN. Creating new PPPoE WAN connection...");
+    if (pppConn.size === 0) {
+        log("GenieACS Auto-Provisioning: Tagged device missing PPPoE WAN. Creating new PPPoE WAN connection...");
 
-    // Create first WANPPPConnection instance under WANConnectionDevice.1
-    declare("InternetGatewayDevice.WANDevice.1.WANConnectionDevice.1.WANPPPConnection.1.Enable", null, { value: true });
-    declare("InternetGatewayDevice.WANDevice.1.WANConnectionDevice.1.WANPPPConnection.1.ConnectionType", null, { value: "IP_Routed" });
-    declare("InternetGatewayDevice.WANDevice.1.WANConnectionDevice.1.WANPPPConnection.1.AddressingType", null, { value: "PPPoE" });
-    declare("InternetGatewayDevice.WANDevice.1.WANConnectionDevice.1.WANPPPConnection.1.NATEnabled", null, { value: true });
-    declare("InternetGatewayDevice.WANDevice.1.WANConnectionDevice.1.WANPPPConnection.1.Username", null, { value: "" });
-    declare("InternetGatewayDevice.WANDevice.1.WANConnectionDevice.1.WANPPPConnection.1.Password", null, { value: "" });
-  }
+        // Create first WANPPPConnection instance under WANConnectionDevice.1
+        declare("InternetGatewayDevice.WANDevice.1.WANConnectionDevice.1.WANPPPConnection.1.Enable", null, { value: true });
+        declare("InternetGatewayDevice.WANDevice.1.WANConnectionDevice.1.WANPPPConnection.1.ConnectionType", null, { value: "IP_Routed" });
+        declare("InternetGatewayDevice.WANDevice.1.WANConnectionDevice.1.WANPPPConnection.1.AddressingType", null, { value: "PPPoE" });
+        declare("InternetGatewayDevice.WANDevice.1.WANConnectionDevice.1.WANPPPConnection.1.NATEnabled", null, { value: true });
+        declare("InternetGatewayDevice.WANDevice.1.WANConnectionDevice.1.WANPPPConnection.1.Username", null, { value: "" });
+        declare("InternetGatewayDevice.WANDevice.1.WANConnectionDevice.1.WANPPPConnection.1.Password", null, { value: "" });
+    }
 }
